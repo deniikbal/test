9fcca9fe15ecd09f4aa37f37f909a9efccc93fd7
deniikbal <deniikbal@gmail.com>
2022-11-27 06:51:37 +0700

    register

--- database/migrations/2022_11_20_135637_create_temp_payments_table.php
+++ database/migrations/2022_11_20_135637_create_temp_payments_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('temp_payments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('student_id')->constrained();
+            $table->string('namestudent');
             $table->timestamps();
         });
     }
